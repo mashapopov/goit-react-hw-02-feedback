508cf8984c1f4a8c856190a924ee5d2aa3e04701
Maria Popova <0979867251m@gmail.com>
2023-03-27 19:31:32 +0300

    phonebook

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -3,17 +3,20 @@ import styled from 'styled-components';
 export const FeedbackList = styled.ul`
   display: flex;
   padding-bottom: 20px;
-  gap: 5px;
+  gap: 20px;
 `;
 
 export const Button = styled.button`
-  font-size: 20px;
+  font-size: 30px;
   background-color: transparent;
-  border: 1px solid hsla(210, 18%, 87%, 1);
-  border-radius: 5px;
+  border: 1px solid hsla(210, 18%, 87%, 2);
+  border-radius: 12px;
   text-transform: capitalize;
   transition: 200ms;
   :active {
+    background-color: blue;
+  }
+  :hover {
     background-color: blue;
   }
 `;
--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const StatisticsReviews = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 5px;
+  gap: 15px;
 `;
 
 export const Span = styled.span`
-  font-size: 20px;
+  font-size: 30px;
 `;
